97f43038eced4eaf9f1d6e0239d55cb8cb5b47b0
quickaxe <manselismyname@gmail.com>
2024-02-14 16:37:31 +0530

    added stuff

--- dataScinece/calc.r
+++ dataScinece/calc.r
@@ -1,18 +1,55 @@
 cat("Enter values of a and b: \n")
 
-a <- as.integer(readLines("stdin", n = 1))
-b <- as.integer(readLines("stdin", n = 1))
+a <- as.numeric(readLines("stdin", n = 1))
+b <- as.numeric(readLines("stdin", n = 1))
 
-cat("Enter : \n1. a+b\n2. a-b\n3. a*b\n4. a/b\n5. a%b\n6. a^b")
-choice <- readLines("stdin", n = 1)
+cat(
+    "Enter operation: \n",
+    "1. a + b\n",
+    "2. a - b\n",
+    "3. a * b\n",
+    "4. a / b\n",
+    "5. a %% b\n",
+    "6. a^b\n",
+    "7. sqrt(a)\n",
+    "8. log(a, b)\n",
+    "9. sin(a)\n",
+    "10. cos(a)\n",
+    "11. tan(a)\n",
+    "12. exp(a)\n",
+    "13. a^2\n",
+    "14. a^3\n",
+    "15. a^(1/2)\n",
+    "16. a^(1/3)\n",
+    "17. log10(a)\n",
+    "18. log2(a)\n",
+    "19. factorial(a)\n",
+    "20. abs(a - b)\n"
+)
+
+choice <- as.integer(readLines("stdin", n = 1))
 
 result <- switch(choice,
-    "1" = paste("sum=", a + b),
-    "2" = paste("difference=", a - b),
-    "3" = paste("product=", a * b),
-    "4" = paste("quotient=", a / b),
-    "5" = paste("remainder=", a %% b),
-    "6" = paste("power=", a**b),
+    "1"  = paste("sum=", a + b),
+    "2"  = paste("difference=", a - b),
+    "3"  = paste("product=", a * b),
+    "4"  = paste("quotient=", a / b),
+    "5"  = paste("remainder=", a %% b),
+    "6"  = paste("power=", a^b),
+    "7"  = paste("square root of a=", sqrt(a)),
+    "8"  = paste("logarithm base b of a=", log(a, b)),
+    "9"  = paste("sine of a=", sin(a)),
+    "10" = paste("cosine of a=", cos(a)),
+    "11" = paste("tangent of a=", tan(a)),
+    "12" = paste("exponential of a=", exp(a)),
+    "13" = paste("a^2=", a^2),
+    "14" = paste("a^3=", a^3),
+    "15" = paste("square root of a=", a^(1 / 2)),
+    "16" = paste("cube root of a=", a^(1 / 3)),
+    "17" = paste("log base 10 of a=", log10(a)),
+    "18" = paste("log base 2 of a=", log2(a)),
+    "19" = paste("factorial of a=", factorial(a)),
+    "20" = paste("absolute difference between a and b=", abs(a - b)),
     "Wrong Choice"
 )
 
--- dataScinece/dataset.r
+++ dataScinece/dataset.r
@@ -1,4 +1,100 @@
 # using inbuilt iris dataset:
 test = iris
 
-test
+# structure of test:
+str(test)
+
+
+minWidth <- min(test$Sepal.Width)
+minLength <- min(test$Sepal.Length)
+minpWidth <- min(test$Petal.Width)
+minpLength <- min(test$Petal.Length)
+
+maxWidth <- max(test$Sepal.Width)
+maxLength <- max(test$Sepal.Length)
+maxpWidth <- max(test$Petal.Width)
+maxpLength <- max(test$Petal.Length)
+
+range <- function(x) {
+    m <- max(x) - min(x)
+    return(m)
+}
+
+range(test$Sepal.Length)
+range(test$Sepal.Width)
+range(test$Petal.Length)
+range(test$Petal.Width)
+
+mean(test$Sepal.Length)
+mean(test$Sepal.Width)
+mean(test$Petal.Length)
+mean(test$Petal.Width)
+
+median(test$Sepal.Length)
+median(test$Sepal.Width)
+median(test$Petal.Length)
+median(test$Petal.Width)
+
+mode <- function(x) {
+    t <- sort(table(x), decreasing = TRUE)[[1]]
+    return(t)
+}
+
+mode(test$Sepal.Length)
+mode(test$Sepal.Width)
+mode(test$Petal.Length)
+mode(test$Petal.Width)
+
+"---------------------------"
+quantile(test$Sepal.Length, 0.25)
+quantile(test$Sepal.Length, 0.50)
+quantile(test$Sepal.Length, 0.75)
+quantile(test$Sepal.Length, 1)
+"---------------------------"
+quantile(test$Sepal.Width, 0.25)
+quantile(test$Sepal.Width, 0.50)
+quantile(test$Sepal.Width, 0.75)
+quantile(test$Sepal.Width, 1)
+"---------------------------"
+quantile(test$Petal.Length, 0.25)
+quantile(test$Petal.Length, 0.50)
+quantile(test$Petal.Length, 0.75)
+quantile(test$Petal.Length, 1)
+"---------------------------"
+quantile(test$Petal.Width, 0.25)
+quantile(test$Petal.Width, 0.50)
+quantile(test$Petal.Width, 0.75)
+quantile(test$Petal.Width, 1)
+
+# inter quantile value
+IQR(test$Sepal.Length)
+IQR(test$Sepal.Width)
+IQR(test$Petal.Length)
+IQR(test$Petal.Width)
+
+#  standard deviation
+sd(test$Sepal.Length)
+sd(test$Sepal.Width)
+sd(test$Petal.Length)
+sd(test$Petal.Width)
+
+"---------------------"
+# variance
+var(test$Sepal.Length)
+var(test$Sepal.Width)
+var(test$Petal.Length)
+var(test$Petal.Width)
+
+"---------------------"
+# all quantile values
+summary(test$Sepal.Length)
+summary(test$Sepal.Width)
+summary(test$Petal.Length)
+summary(test$Petal.Width)
+
+"---------------------"
+# corellation
+(test$Sepal.Length)
+(test$Sepal.Width)
+(test$Petal.Length)
+(test$Petal.Width)
